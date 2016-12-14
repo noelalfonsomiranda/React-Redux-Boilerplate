ede20897ddebb68ad0cc103cbd9520e9698f1754
zjonez <jonas.zapanta@gmail.com>
2016-12-14 10:43:07 +0800

    Lesson 6 - Providers

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,9 +1,15 @@
 import 'babel-polyfill';
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import allReducers from './reducers';
+import App from './components/App';
 
 const store = createStore(allReducers);
 
-ReactDOM.render(<h1>Boom roasted</h1>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
